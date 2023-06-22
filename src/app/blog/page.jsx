4806925ae0415f04259bb4404c5e806c6309fe25
BastienAuT <deh2win@gmail.com>
2023-06-22 23:35:31 +0200

    trying to fix errors

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+"use client"
+import React, { useEffect, useState } from "react";
 import styles from "./page.module.css";
 import Link from "next/link";
 import Image from "next/image";
@@ -18,13 +19,30 @@ async function getData() {
   return res.json();
 }
 
-const Blog = async () => {
-  const data = await getData();
+const Blog = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const result = await getData();
+        setData(result);
+      } catch (error) {
+        console.error("Failed to fetch data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <div className={styles.mainContainer}>
       {data.map((item) => (
-        <Link href={`/blog/${item._id}`} className={styles.container} key={item.id}>
+        <Link
+          href={`/blog/${item._id}`}
+          className={styles.container}
+          key={item.id}
+        >
           <div className={styles.imageContainer}>
             <Image
               src={item.img}
@@ -45,3 +63,51 @@ const Blog = async () => {
 };
 
 export default Blog;
+
+// import React from "react";
+// import styles from "./page.module.css";
+// import Link from "next/link";
+// import Image from "next/image";
+
+// async function getData() {
+//   const res = await fetch(
+//     "https://luminous-melba-106563.netlify.app/api/posts",
+//     {
+//       cache: "no-store",
+//     }
+//   );
+
+//   if (!res.ok) {
+//     throw new Error("Failed to fetch data");
+//   }
+
+//   return res.json();
+// }
+
+// const Blog = async () => {
+//   const data = await getData();
+
+//   return (
+//     <div className={styles.mainContainer}>
+//       {data.map((item) => (
+//         <Link href={`/blog/${item._id}`} className={styles.container} key={item.id}>
+//           <div className={styles.imageContainer}>
+//             <Image
+//               src={item.img}
+//               alt="ui"
+//               width={400}
+//               height={250}
+//               className={styles.image}
+//             />
+//           </div>
+//           <div className={styles.content}>
+//             <h1 className={styles.title}>{item.title}</h1>
+//             <p className={styles.desc}>{item.desc}</p>
+//           </div>
+//         </Link>
+//       ))}
+//     </div>
+//   );
+// };
+
+// export default Blog;
