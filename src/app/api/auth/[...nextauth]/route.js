79175c5691a6b962668cab4fa747759d5f0f47ec
BastienAuT <deh2win@gmail.com>
2023-06-23 15:05:40 +0200

    test

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,17 +1,12 @@
-import User from "@/models/User";
-import connect from "@/utils/db";
-import NextAuth from "next-auth/next";
+import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import CredentialsProvider from "next-auth/providers/credentials";
+import User from "@/models/User";
+import connect from "@/utils/db";
 import bcrypt from "bcryptjs";
 
 const handler = NextAuth({
   providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    }),
-
     CredentialsProvider({
       id: "credentials",
       name: "Credentials",
@@ -42,6 +37,10 @@ const handler = NextAuth({
           throw new Error(err);
         }
       },
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
   pages: {
